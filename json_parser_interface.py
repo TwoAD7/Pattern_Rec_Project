e70a71f64ce1f6c74cd5b9fc40a049d61381cc4e
Roy Salinas <Roy-123456@hotmail.com>
2020-04-19 15:33:00 -0500

    Parsing interface to run in shell script

--- json_parser_interface.py
+++ json_parser_interface.py
@@ -2,10 +2,8 @@ import json_parser_module as jp
 import sys 
 
 
-#colors are the colors we are looking for
 inputfile = sys.argv[1] #passed in by shell script
 outfile = sys.argv[2]	#^
-#inputfile="./test.json"
 #For colors, insert the colors you are interested in finding in the .json file
 colors = ["red","green"]
 jp.parser(inputfile,outfile,colors)
