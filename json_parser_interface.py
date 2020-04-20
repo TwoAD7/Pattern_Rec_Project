f278201af9408fe88d7842c31e49f73d76c20eb6
Roy Salinas <Roy-123456@hotmail.com>
2020-04-19 22:52:43 -0500

    Update json_parser_interface.py

--- json_parser_interface.py
+++ json_parser_interface.py
@@ -5,5 +5,7 @@ import sys
 inputfile = sys.argv[1] #passed in by shell script
 outfile = sys.argv[2]	#^
 #For colors, insert the colors you are interested in finding in the .json file
-colors = ["red","green"]
+color_input = color_input.split() #returns array 
+colors = color_input
+
 jp.parser(inputfile,outfile,colors)
