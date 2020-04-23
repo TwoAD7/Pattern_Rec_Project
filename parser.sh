c075cd24b19000817fe37e0162d19d732928fd0c
Roy Salinas <Roy-123456@hotmail.com>
2020-04-22 23:30:11 -0500

    Update parser.sh

--- parser.sh
+++ parser.sh
@@ -1,11 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
 #Roy Salinas
 #Use this scrip to parse through the .json files 
-
-filedirectory="/home/pi/ext_drive/train.json" #change this to the directory we are looking into. Still needs to be incorporated for the smaller partitions (Roy - 04.17.20)
-outfile="test_out.txt"
+filedirectory="/mnt/c/Users/Owner/Desktop/Annotations/train.json"
 script1=json_parser_module.py
 script2=json_parser_interface.py
+   
+echo -n "Enter the total number of colors you are looking for:"
+read n 
+echo "Enter colors followed by the 'Enter' key after each one :"
+i=0 
+  
+# Read upto the size of  
+# given array starting from 
+# index, i=0 
+while [ $i -lt $n ]; do
+    # To input from user 
+    read a[$i] 
+  
+    # Increment the i = i + 1 
+    i=`expr $i + 1` 
+done
+
+printf -v outfile "%s," "${a[@]}.txt" 
+outfile=${outfile%?}  #to get rid of the last comma 
+#echo ${outfile}
+echo "The colors you entered are ${a[@]}"
 echo "You are running $script1 and $script2"
-python3 $script2 "$filedirectory" "$outfile"
+#pass in the input file, outfile, and the colors we are looking for
+python3 $script2 "$filedirectory" "$outfile" ${a[@]}
