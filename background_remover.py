a2217512adb706748318d0dd000c8c1dabd7478c
keatonbrewster <40281370+keatonbrewster@users.noreply.github.com>
2020-04-24 10:45:10 -0500

    Update background_remover.py

--- background_remover.py
+++ background_remover.py
@@ -40,8 +40,8 @@ img = cv2.imread('chicken.jpg')
 
 # setting up contours____________________________________________
 gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-gray_img = cv2.bitwise_not(gray_img) # flips which is foreground and background
-_, thresh = cv2.threshold(gray_img,5,5,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+# gray_img = cv2.bitwise_not(gray_img) # flips which is foreground and background
+_, thresh = cv2.threshold(gray_img,155,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 img_contours = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[-2]
 
 # cv2_imshow(gray_img)
