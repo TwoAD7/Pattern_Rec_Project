e30f519bb9d0c7a845d84f8a8d07b50adeed33d3
Roy Salinas <Roy-123456@hotmail.com>
2020-05-04 14:27:13 -0500

    Update color_average_single.py
    To read in a single image to the RPI for individual image information

--- color_average_single.py
+++ color_average_single.py
@@ -133,8 +133,7 @@ def color_average(rootdir):
         #print(f"{d[0]} {d[1]} {d[2]} {d[3]} {d[4]} {d[5]}")
         fout.write(f"{d[0]} {d[1]} {d[2]} {d[3]} {d[4]} \n")
 
-        #save_all.append(d) #save file name and color information 
-        #print(save_all)
+  
         counter = counter+1
         print(f"You have gone through {counter} images.")
     print(f"The name of the file as it appears is {rootdir}_testing_colorinfo.txt ")
