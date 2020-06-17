057b12ff210ca4af7df14375eaadd255deaf851e
Roy Salinas <Roy-123456@hotmail.com>
2020-06-17 13:10:30 -0500

    Update json_parser_module.py

--- json_parser_module.py
+++ json_parser_module.py
@@ -3,7 +3,7 @@ import sys
 
 #Written by: Roy Salinas
 #Purpose: Module to parse through the .json file
-
+#Arguments come from json_parser_interface.sh
 
 if __name__ == "__main__":
 	print("Runnning the parser in terminal!")
