a648d898a11ba6ec5d97cb635c8bf390ed4e78a5
Roy Salinas <Roy-123456@hotmail.com>
2020-06-22 12:17:21 -0500

    Update json_parser_interface.py

--- json_parser_interface.py
+++ json_parser_interface.py
@@ -6,6 +6,8 @@ import sys
 #All arguments come from the shell script "parser.sh"
 
 #Colors are the colors we are looking for
+#sys.argv[0] is the name of this script, doesn't make sense to pass that in 
+#as that is THIS script
 inputfile = sys.argv[1] #passed in by shell script
 outfile = sys.argv[2]	#^
 colors  = sys.argv[3:] #grabbing all of the arguments in bash array in the third index of input
